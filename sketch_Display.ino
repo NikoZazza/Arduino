c49839a6e542686d05bda3395ad55cad61a57bc9
xionbig <nikola.zazza@gmail.com>
2016-04-14 16:52:46 +0200

    Added files via upload

---
+++ sketch_Display.ino
@@ -0,0 +1,42 @@
+int PIN_LUX = A0; /*  Questa variabile indica la porta di aruino dove dobbiamo
+                   *  leggere il valore (Analogico) del fotoresistore. Usare questa variabile
+                   *  semplifica molto la riusabilità del codice, infatti se cambiamo la porta
+                   *  del sensore, basta cambiare questa variabile.    
+                   */
+int val = 0;      /*  Questa variabile serve a memorizzare il valore letto dal sensore
+                   *  collegato alla porta PIN_LUX. Questo valore analogico ha un range da 0 a 1024.
+                   */
+#include <LiquidCrystal.h>  // Qui importiamo la libreria del display lcd.
+                             
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);  // Dichiariamo la variabile lcd specificando le porte che sono connessi al display
+/*
+ * Questa funzione è chiamata una volta sola quando il programma viene fatto funzionare. 
+ * E’ usata per inizializzare il pinMode e deve essere incluso in un programma, anche se non ci sono istruzioni da eseguire.
+ * La funzione pinMode deve essere utilizzata SOLO per le porte digitali, per le porte analogiche non bisogna effettuare alcuna 
+ * modifica al programma, basta solo fare analogRead().
+ */
+void setup() {
+  lcd.begin(16, 2);   /* Indichiamo la grandezza del nostro lcd. Ogni lcd è considerato come una matrice, in questo caso 
+                       * una matrice 16x2, cioè il nostro display è composto da due righe dove ogni riga può contenere al massimo 16 caratteri.                      *
+                       */
+  lcd.setCursor(0, 0);  // Indichiamo la posizione dove vogliamo scrivere qualcosa, in questo caso puntiamo la prima riga e la prima lettera.
+  lcd.print("Gruppo n.5");  // Scrive sul display a partire dalla posizione 0 e dalla prima riga "Gruppo n.5".
+  delay(2000); // Attendiamo 2sec prima che il programma inizi a leggere i valori dal fotosensore.
+}
+/*  Questa funzione (che deve essere presente in ogni programma) viene eseguita di continuo da arduino, 
+ *  finche non lo scolleghi dalla corrente. Considerando che Arduino va a 16.000Mhz, in 1ms questa funzione   
+ *  verrà eseguita 16.000 volte, quindi in un secondo questa funzione verrà eseguita 16.000.000 volte.
+ */
+void loop() {
+  val = analogRead(PIN_LUX); // Leggiamo il valore analogico della porta PIN_LUX e lo inseriamo nella variabile val
+  lcd.setCursor(0, 2);  // Indichiamo la posizione dove vogliamo scrivere qualcosa, in questo caso puntiamo la seconda riga e la prima lettera. 
+  lcd.print("               ");  /* Cancelliamo i valori stampati precedentemente cosi il valore letto sarà quello reale. Da notare lo spazio visto che 
+                                  * dobbiamo cancellare il vecchio valore e scriverne un'altro, i caratteri vuoti verranno scritti sul display. (Ci sono 16 spazi)
+                                  */
+  lcd.setCursor(0, 2); // Indichiamo la posizione dove vogliamo scrivere qualcosa, in questo caso puntiamo la seconda riga e la prima lettera.  
+  lcd.print(val); // Scriviamo sul display il valore prelevato nella seconda riga 
+  delay(300); /* Aspettiamo 300ms di secondo, serve questa pausa visto che al convertitore Analogico/Digitale servono circa 20ms
+               * solo per prelevare un valore senza avere dei "difetti". Il valore impostato a 300ms permette la visualizzazione
+               * della variazione della luminosità.
+               */
+}
---
+++ sketch_Semaforo.ino
@@ -0,0 +1,59 @@
+int PIN_BTN = 7; /* Questa variabile indica la porta di aruino dove dobbiamo
+                  * verificare se il buttone è premuto. Usare questa variabile
+                  * semplifica molto la riusabilità del codice, infatti se cambiamo la porta
+                  *  del sensore, basta cambiare questa variabile.    
+                  */ 
+
+int PIN_R = 3; // Indica la porta dove è collegato il led Rosso
+int PIN_Y = 4; // Indica la porta dove è collegato il led Giallo
+int PIN_G = 5; // Indica la porta dove è collegato il led Verde
+
+int state = 0; /* Indica lo stato corrente, varia da 0 a 2 e significa: 
+*/
+
+/*
+ * Questa funzione è chiamata una volta sola quando il programma viene fatto funzionare. 
+ * E’ usata per inizializzare il pinMode e deve essere incluso in un programma, anche se non ci sono istruzioni da eseguire.
+ * La funzione pinMode deve essere utilizzata SOLO per le porte digitali, per le porte analogiche non bisogna effettuare alcuna 
+ * modifica al programma, basta solo fare analogRead().
+ */
+void setup() {
+  pinMode(PIN_BTN, INPUT); // Imposta il pin del buttone come INPUT
+  pinMode(PIN_R, OUTPUT); // Imposta il pin del buttone come OUTPUT
+  pinMode(PIN_Y, OUTPUT); // Imposta il pin del buttone come OUTPUT
+  pinMode(PIN_G, OUTPUT); // Imposta il pin del buttone come OUTPUT
+}
+/*  Questa funzione (che deve essere presente in ogni programma) viene eseguita di continuo da arduino, 
+ *  finche non lo scolleghi dalla corrente. Considerando che Arduino va a 16.000Mhz, in 1ms questa funzione   
+ *  verrà eseguita 16.000 volte, quindi in un secondo questa funzione verrà eseguita 16.000.000 volte.
+ */
+void loop() {
+  if(digitalRead(PIN_BTN) == HIGH){ /* Controlliamo se il buttone è premuto, 
+                                     * se è premuto restituisce HIGH (+5v), altrimenti se non è premuto restituisce LOW (-0v).
+                                     * La funzione digitalRead è composta solo da un parametro: la porta digitale su cui vogliamo leggere il valore.
+                                     * Se la porta digitale non è stata impostata su INPUT con la funzione pinMode, il programma non funzionerà.
+                                      */
+    state++; 
+    delay(100); // Aspettiamo 100ms di secondo, se non aspettiamo, il programma andrebbe cosi veloce da cambiare lo stato dei led in brevissimo tempo.
+  }
+  if(state >= 3) // Se è acceso il led Verde, azzerra la variabile cosi si accenderà il led Rosso.
+    state = 0;
+  switch(state){ // In base allo stato della variabile, esegue una serie di funzioni
+    case 0:
+      digitalWrite(PIN_R, HIGH); // Accendiamo il led Rosso
+      digitalWrite(PIN_Y, LOW); // Spegniamo il led Giallo
+      digitalWrite(PIN_G, LOW); // Spegniamo il led Verde
+      break;
+    case 1:
+      digitalWrite(PIN_R, LOW); // Spegniamo il led Rosso
+      digitalWrite(PIN_Y, HIGH); // Accendiamo il led Giallo
+      digitalWrite(PIN_G, LOW); // Spegniamo il led Verde
+      break;
+    case 2:
+      digitalWrite(PIN_R, LOW); // Spegniamo il led Rosso
+      digitalWrite(PIN_Y, LOW); // Spegniamo il led Giallo
+      digitalWrite(PIN_G, HIGH); // Accendiamo il led Verde
+      break;
+  }
+  delay(100); // Aspettiamo 100ms di secondo.
+}
